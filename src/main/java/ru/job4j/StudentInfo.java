c15c4d65abe279d732730b3fc2ea0f51c66a59aa
Efi-Min <alxefimin@gmail.com>
2022-05-08 23:48:22 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alx Efi_Min");
+        System.out.println("04.06.2001");
+    }
+}
